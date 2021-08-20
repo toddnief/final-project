c16f82269f6e84e7a4cb8c2b9bb36805854e01db
toddnief <todd.nief@gmail.com>
2021-08-20 11:10:08 -0500

    clean up and refactor

--- final-project.py
+++ final-project.py
@@ -1,148 +1,6 @@
 
-# The user should run the program completely from the command line passing in commands and arguments that will alter the behavior of the program. The commands are --add, --delete, --list, --report, --query, and --done. Use the argparse package to help with parsing the command line arguements.
-
-# Each command will be followed with arguments, as seen in the following examples:
-
-# Task Add
-# Add a new task by using the --add command. Examples of adding tasks are shown below.
-
-# Note that the task description needs to be enclosed in quotes if there are mulitple words. argparse should provide sufficient error handling for user input, however you need to test that the data matches the type expected.
-
-# The unique identifier is returned when the operation is successful. If the operation is not successful, inform the user and end the program. Remember that the due date is optional. If a priority is not given, then assign it a default value of 1.
-
-# $ python todo.py --add "Walk Dog" --due 4/17/2018 --priority 1
-# Created task 1
-
-# $ python todo.py --add 2 --due 4/17/2018 --priority 1
-# There was an error in creating your task. Run "todo -h" for usage instructions.
-
-# $ python todo.py --add "Study for finals" --due 3/20/2018 --priority 3
-# Created task 2
-
-# $ python todo.py --add "Buy milk and eggs" —due friday --priority 2
-# Created task 3
-
-# $ python todo.py --add "Cook eggs"
-# Created task 4
-
-import sys
 import tasks_helper
-
-def add_task(args, all_tasks):
-    name = args.add
-    task = tasks_helper.Task(name)
-
-    if args.priority:
-        task.priority = args.priority
-    if args.due:
-        task.due = args.due
-
-    all_tasks.add(task)
-    print(f"Created task {task.id}")
-
-    return
-
-# Task List Command
-# Use the --list command to display a list of the not completed tasks sorted by the due date. If tasks have the same due date, sort by decreasing priority (1 is the highest priority). If tasks have no due date, then sort by decreasing priority.
-
-# Note that only tasks that are not completed should be listed with this command. The Age in the table is the number of days since the task was created.
-
-# Follow the formatting shown below.
-
-# $ python todo.py list
-
-# ID   Age  Due Date   Priority   Task
-# --   ---  --------   --------   ----
-# 1    3d   4/17/2018   1         Walk dog
-# 2    10d  3/20/2018   3         Study for finals
-# 3    1d   -           1         Buy eggs
-# 4    30d  -           2         Make eggs
-
-
-# Task List Command Using a Query Term
-# Search for tasks that match a search term using the --query command. Only return tasks are not completed in your results.
-
-# $ python todo.py --query eggs
-
-# ID   Age  Due Date   Priority   Task
-# --   ---  --------   --------   ----
-# 3    1d   -           2         Buy eggs
-# 4    30d  -           1         Make eggs
-
-# Muliple terms should be able to be searched. The argparse package allows you to pass in multiple values for a single argument using nargs='+':
-
-# parser.add_argument('--query', type=str, required=False, nargs="+", help="priority of task; default value is 1")
-# For example:
-
-# $ python todo.py --query eggs dog
-
-# ID   Age  Due Date   Priority   Task
-# --   ---  --------   --------   ----
-# 1    3d   4/17/2018   1         Walk dog
-# 3    1d   -           2         Buy eggs
-# 4    30d  -           1         Make eggs
-
-def task_query(args, tasks):
-    pass
-
-# Task Done Command
-# Complete a task by passing the done argument and the unique identifier. The following example complete tasks 1 and 2. Remember that you are not deleting a task, you are just marking it as complete. Your --list methods should ensure that it not longer is printed to the terminal.
-
-# $ python todo.py --done 1
-# Completed task 1
-
-# $ python todo.py --done 2
-# Completed task 2
-
-# $ python todo.py --list
-
-# ID   Age  Due Date   Priority   Task
-# --   ---  --------   --------   ----
-# 3    1d   -           2         Buy eggs
-# 4    30d  -           1         Make eggs
-
-def complete_task(id):
-    pass
-
-# Delete Command
-# Delete a task by passing the --delete command and the unique identifier.
-
-# $ python todo.py --delete 3
-# Deleted task 3
-
-# $ python todo.py list
-
-# ID   Age  Due Date   Priority   Task
-# --   ---  --------   --------   ----
-# 4    30d  -           1         Make eggs
-
-def delete_task(args):
-    pass
-
-# Task Report Command
-# List all tasks, including both completed and incomplete tasks, using the report command. Follow the formatting shown below for the the output. Follow the same reporting order as the --list command.
-
-# $ python todo.py report
-
-# ID   Age  Due Date   Priority   Task                Created                       Completed
-# --   ---  --------   --------   ----                ---------------------------   -------------------------
-# 1    3d   4/17/2018   1         Walk dog            Mon Mar  5 12:10:08 CST 2018  Mon Mar  5 12:10:08 CST 2018
-# 2    10d  3/20/2018   3         Study for finals    Tue Mar  6 12:10:08 CST 2018  Tue Mar  6 12:10:08 CST 2018
-# 3    1d   -           2         Buy eggs            Tue Mar  6 12:10:08 CST 2018  -
-# 4    30d  -           1         Make eggs           Tue Mar  6 12:10:08 CST 2018  -
-# Note: This action will be useful for debugging and testing the completed and deleted commands.
-
-def task_report(tasks):
-    pass
-
-# Make your task manager program an executable program. This will allow it to be run from any location on your computer. You will need to place a copy of it in a place where $PATH is looking for executable files. Research to find out where command line applications are typically stored (this will vary betweren Macs, Linus and Windows). You will also need change the running mode of the file (ie. chmod) and add a shebang line. You will also need to change the location of the users .pickle file so that it can be accessed from anywhere. Store it as an invisible file in the users home directory.
-
-# Write the instructions for how you can accomplish this in a README.md file in your repository.
-
 import argparse
-
-# The user should run the program completely from the command line passing in commands and arguments that will alter the behavior of the program. The commands are --add, --delete, --list, --report, --query, and --done. Use the argparse package to help with parsing the command line arguements.
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Command Line Task Manager.')
@@ -183,7 +41,7 @@ if __name__ == "__main__":
     all_tasks = tasks_helper.Tasks()
 
     if args.add:
-        add_task(args, all_tasks)
+        all_tasks.add(args)
     elif args.list:
         all_tasks.list()
     elif args.delete:
@@ -191,13 +49,11 @@ if __name__ == "__main__":
     elif args.query:
         all_tasks.query(args)
     elif args.done:
-        complete_task(args.done)
+        all_tasks.done(args.done)
 
+    # Pickle all tasks
     all_tasks.pickle_tasks()
 
-    # task1 = tasks.Task("help me", 1, "4/17/2018")
+# Make your task manager program an executable program. This will allow it to be run from any location on your computer. You will need to place a copy of it in a place where $PATH is looking for executable files. Research to find out where command line applications are typically stored (this will vary betweren Macs, Linus and Windows). You will also need change the running mode of the file (ie. chmod) and add a shebang line. You will also need to change the location of the users .pickle file so that it can be accessed from anywhere. Store it as an invisible file in the users home directory.
 
-    # if not args:
-    #     print('Please include the file you wish to rename after the problem6.py command')
-    #     sys.exit(1)
-
+# Write the instructions for how you can accomplish this in a README.md file in your repository.
--- tasks_helper.py
+++ tasks_helper.py
@@ -1,7 +1,4 @@
-# Each task should be able to be uniquely identified from all other tasks by a numeric identifier. Tasks should be assigned a priority level of 1, 2 or 3 to indicate the importance (3 is the highest priority).
-
 from datetime import date
-import time
 import pickle
 from dateutil import parser
 from pathlib import Path
@@ -19,9 +16,10 @@ class Task:
     """
     def __init__(self, name, priority=None, due=None):
         self.name = name
+        self.completed = 0
         self.created = date.today()
 
-        if priority:
+        if priority==1 or priority==2 or priority==3:
             self.priority = priority
         else:
             self.priority = 1
@@ -37,8 +35,6 @@ class Task:
 
         return print_version
 
-# Implement a Tasks objects that will contain all the Task objects. Tasks should be implemented as a list of Task objects. You may use the standard Python list() to hold you Task objects. The list should be ordered by the creation date (to improve search efficiency). While running, you program should only have a single instance of Tasks.
-
 class Tasks:
     """A list of `Task` objects."""
 
@@ -46,9 +42,9 @@ class Tasks:
         """Read pickled tasks file into a list"""
         self.pickle_name = ".todo.pickle"
 
-        # List of Task objects
         self.tasks = []
 
+        # read pickled tasks. Create pickle file if file not found.
         try:
             self.tasks = pickle.load( open(self.pickle_name, "rb" ) )
         except:
@@ -62,16 +58,64 @@ class Tasks:
         pickle.dump(self.tasks ,open(self.pickle_name,"wb" ))
 
     def list(self):
+        """Print tasks in columns."""
         print("ID\tAge\tDue Date\tPriority\tTask")
         print("--\t---\t--------\t--------\t----")
         for task in self.tasks:
-            print(f"{task}")
+            if task.completed == 0:
+                print(f"{task}")
+
+# Task Report Command
+# List all tasks, including both completed and incomplete tasks, using the report command. Follow the formatting shown below for the the output. Follow the same reporting order as the --list command.
+
+# $ python todo.py report
+
+# ID   Age  Due Date   Priority   Task                Created                       Completed
+# --   ---  --------   --------   ----                ---------------------------   -------------------------
+# 1    3d   4/17/2018   1         Walk dog            Mon Mar  5 12:10:08 CST 2018  Mon Mar  5 12:10:08 CST 2018
+# 2    10d  3/20/2018   3         Study for finals    Tue Mar  6 12:10:08 CST 2018  Tue Mar  6 12:10:08 CST 2018
+# 3    1d   -           2         Buy eggs            Tue Mar  6 12:10:08 CST 2018  -
+# 4    30d  -           1         Make eggs           Tue Mar  6 12:10:08 CST 2018  -
+# Note: This action will be useful for debugging and testing the completed and deleted commands.
 
     def report(self):
         pass
 
-    def done(self):
-        pass
+    def done(self, id):
+        """Find task by id and mark as done."""
+        i = 0
+        for task in self.tasks:
+            if task.id == id:
+                task.completed = 1
+                print(f"Completed Task {id}")
+                break
+            i += 1
+        if i == len(self.tasks):
+            print(f"Could not find task with id {id}")
+        return
+
+# Task List Command Using a Query Term
+# Search for tasks that match a search term using the --query command. Only return tasks are not completed in your results.
+
+# $ python todo.py --query eggs
+
+# ID   Age  Due Date   Priority   Task
+# --   ---  --------   --------   ----
+# 3    1d   -           2         Buy eggs
+# 4    30d  -           1         Make eggs
+
+# Muliple terms should be able to be searched. The argparse package allows you to pass in multiple values for a single argument using nargs='+':
+
+# parser.add_argument('--query', type=str, required=False, nargs="+", help="priority of task; default value is 1")
+# For example:
+
+# $ python todo.py --query eggs dog
+
+# ID   Age  Due Date   Priority   Task
+# --   ---  --------   --------   ----
+# 1    3d   4/17/2018   1         Walk dog
+# 3    1d   -           2         Buy eggs
+# 4    30d  -           1         Make eggs
 
     def query(self, args):
         query_matches = []
@@ -82,6 +126,7 @@ class Tasks:
         return query_matches
 
     def delete(self,id):
+        """Find task by id and delete it."""
         i = 0
         for task in self.tasks:
             if task.id == id:
@@ -93,13 +138,49 @@ class Tasks:
             print(f"Could not find task with id {id}")
         return
 
-    def add(self, task):
+# Task Add
+# Add a new task by using the --add command. Examples of adding tasks are shown below.
+
+# Note that the task description needs to be enclosed in quotes if there are mulitple words. argparse should provide sufficient error handling for user input, however you need to test that the data matches the type expected.
+
+# The unique identifier is returned when the operation is successful. If the operation is not successful, inform the user and end the program. Remember that the due date is optional. If a priority is not given, then assign it a default value of 1.
+
+# $ python todo.py --add "Walk Dog" --due 4/17/2018 --priority 1
+# Created task 1
+
+# $ python todo.py --add 2 --due 4/17/2018 --priority 1
+# There was an error in creating your task. Run "todo -h" for usage instructions.
+
+# $ python todo.py --add "Study for finals" --due 3/20/2018 --priority 3
+# Created task 2
+
+# $ python todo.py --add "Buy milk and eggs" —due friday --priority 2
+# Created task 3
+
+# $ python todo.py --add "Cook eggs"
+# Created task 4
+
+    def add(self, args):
+        """Add task to task list. Create task id one larger than current largest task id."""
+        # Create task object with task name.
+        name = args.add
+        task = Task(name)
+
+        # Set task value for priority and due date if arguments provided.
+        if args.priority:
+            task.priority = args.priority
+        if args.due:
+            task.due = args.due
+
+        # Initialize task IDs at 1. Otherwise increment by 1 from largest current ID.
+        if self.tasks == []:
+            largest_id = 0
+        else:
+            largest_id = max(task.id for task in self.tasks)
+        task.id = largest_id + 1
+
+        # Add new task to tasks list.
         self.tasks.append(task)
-        task.id = len(self.tasks)
+
+        print(f"Created Task {task.id}")
         return
-
-# All the Tasks data should be serialized to disk using Python pickle module. Write to a file named .todo.pickle in the same directory as your program. The . is not a typo. Prefixing a file with a . makes it an invisible file.
-
-# This file will exist in a directory, but will not be visible to the user by default. This is a good practice when storing data in a file that you do not want the user to open or modify. See unix challenge on how to visualize invisible files.
-
-# When your program exits, you should ensure that all tasks have been serialized to disk. When you program begins, you should ensure that all your tasks are loaded into objects from the serialized file.
